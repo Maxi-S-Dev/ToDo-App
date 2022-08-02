709ef820eaeb015d227a66267bcfcfef2cfade79
Majost <57063653+Maximilian-Strigl@users.noreply.github.com>
2022-08-02 20:19:24 +0200

    Basic Task Creation

--- ToDoApp/ToDoApp/App.xaml.cs
+++ ToDoApp/ToDoApp/App.xaml.cs
@@ -8,4 +8,16 @@ public partial class App : Application
 
 		MainPage = new MainPage();
 	}
+
+	protected override Window CreateWindow(IActivationState activationState)
+	{
+		Window window = base.CreateWindow(activationState);
+		if(window != null)
+		{
+			window.Title = "";
+
+		}
+
+		return window;
+	}
 }
---
+++ ToDoApp/ToDoApp/Data/Models/TaskModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Data.Models
+{
+    public class TaskModel
+    {
+        public string name { get; set; }
+        public bool isCompleted { get; set; }
+
+        public bool mouseOver;
+    }
+}
